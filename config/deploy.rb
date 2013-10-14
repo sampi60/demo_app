4873b78807566254dca1054aafec8a2fb7398ab0
Piotr Sampławski <sampi@gumed.edu.pl>
2013-10-14 09:09:38 +0200

    Fighting with capistrano and alwaysdata

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,33 @@
 set :user, "sampi"
 set :scm_username, "sampi60"
 set :application, "demo_app"
-set :deploy_to, "/home/sampi/www/#{application}"
+set :deploy_to, "/home/#{user}/www/#{application}"
 set :repository, "https://github.com/#{scm_username}/#{application}.git"
 
+
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :branch, "capi"
+
 set :use_sudo, false
+ssh_options[:forward_agent] = true
+set :deploy_via, :remote_cache
 
 set :domain, "ssh.alwaysdata.com"
 server domain, :app, :web
 role :db, domain, :primary => true
-
+set :runner, user
 # role :web, "your web-server here"                          # Your HTTP server, Apache/etc
 # role :app, "your app-server here"                          # This may be the same as your `Web` server
 # role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
+
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
